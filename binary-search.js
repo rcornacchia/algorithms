8247925bbefc2becad36c759f4ea5359f123d328
rcornacchia <rlc2160@columbia.edu>
2018-03-03 17:43:01 -0500

    finished binary search

--- binary-search.js
+++ binary-search.js
@@ -1,28 +1,27 @@
 // given a sorted array of integers return the index of the given key or -1 if not found.
 
-function binarySearch(arr, key, start, end) {
-  if (!start || !end) {
-    start = 0
-    end = arr.length-1
-  }
-  if (start - end == 0) {
-    if (arr[start] = key) return start
-    else return -1
-  }
+function binarySearch(arr, key, start=0, end=arr.length-1) {
 
-  const midIndex = Math.floor((end - start) / 2)
-  let mid = arr[midIndex]
-  console.log(mid)
-  console.log(midIndex)
+
+  const midIndex = Math.floor((end + start) / 2)
+  const mid = arr[midIndex]
 
   if (mid == key) return midIndex
+  if (start - end == 0) {
+    return -1
+  }
 
   if (mid > key) {
-    binarySearch(arr, key, start, midIndex-1)
+    return binarySearch(arr, key, start, midIndex-1)
   } else {
-    binarySearch(arr.slice(midIndex+1, arr.length), key)
+    return binarySearch(arr, key, midIndex + 1, end)
   }
 }
 
 const a = [1, 5, 8, 9, 19, 199, 390]
-console.log(binarySearch(a, 5))
+
+console.log(binarySearch(a, 1))   // 0
+console.log(binarySearch(a, 5))   // 1
+console.log(binarySearch(a, 9))   // 3
+console.log(binarySearch(a, 199)) // 5
+console.log(binarySearch(a, 15))  // -1
