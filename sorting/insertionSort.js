4eefba808eb84c13d8c7813da7fa5d85fbcab847
rcornacchia <rlc2160@columbia.edu>
2018-03-08 18:49:01 -0500

    added insertion sort

---
+++ sorting/insertionSort.js
@@ -0,0 +1,17 @@
+const insertionSort = arr => {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > key) {
+      arr[j+1] = arr[j]
+      j--
+    }
+    arr[j+1] = key
+  }
+  return arr
+}
+
+const unsorted = [5, 3, 8, 9, 2, 1, 7]
+const sorted = insertionSort(unsorted)
+console.log(sorted)
