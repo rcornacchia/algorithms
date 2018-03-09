777a634686af5db3c63d30403406f3636a6736aa
rcornacchia <rlc2160@columbia.edu>
2018-03-09 09:21:02 -0500

    further work on merge sort

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,11 +1,29 @@
-// consists of two functions:
 // merge-sort recursively calls itself passing in both halfs of the array
-// merge takes two sorted arrays and merges them together
-
-const mergeSort = arr => {
-
+const mergeSort = (arr, start, mid, end) => {
+  if (mid > start) {
+    let mid = Math.floor((end+start) / 2)
+    mergeSort(arr, start, mid)
+    mergeSort(arr, mid+1, end)
+    merge(arr, start, mid, end)
+  }
 }
 
-const merge = arr => {
-  
+
+// given two sorted arrays, merge them together
+const merge = (arr, start, mid, end) => {
+  let result = []
+  let left = arr.slice(start, mid+1)
+  let right = arr.slice(mid+1, end+1)
+  let i = 0
+  while (left.length || right.length) {
+    if (!left.length) result.concat.apply(result, right)
+    if (!right.length) result.push (...left)
+    (left[0] < right[0])
+      ? result.push(left.pop())
+      : result.push(right.pop())
+  }
+  return result
 }
+
+const result = merge([1, 5, 7, 2, 4, 6, 10], 0, 3)
+console.log(result)
